1e959c798d8a6921e8f31d3ca5696e516db12a91
Andrew <travers.drew@gmail.com>
2021-02-04 23:55:39 -0500

    Create tweet form & view

--- tweets/views.py
+++ tweets/views.py
@@ -1,12 +1,24 @@
 import random
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Tweet
+from .forms import TweetForm
 
 def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Hello World</h1>")
     return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        if next_url != None:
+            return redirect(next_url)
+        form = TweetForm()
+    return render(request, 'components/forms.html', context={"form": form})
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
