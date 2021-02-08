50d8411f79a37a3c542f745bd1636089c285ee04
Andrew <travers.drew@gmail.com>
2021-02-07 23:41:04 -0500

    Add user

--- tweets/models.py
+++ tweets/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.conf import settings
 
+User = settings.AUTH_USER_MODEL
 
 class Tweet(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images.', blank=True, null=True)
 
