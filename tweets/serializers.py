b00dc2a5b84e2a2c40a7e1f2a5569f74a46677fd
Andrew <travers.drew@gmail.com>
2021-02-17 16:53:33 -0500

    Tweet serializer

--- tweets/serializers.py
+++ tweets/serializers.py
@@ -16,9 +16,13 @@ class TweetActionSerializer(serializers.Serializer):
         return value
 
 class TweetSerializer(serializers.ModelSerializer):
+    likes = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Tweet
-        fields = ['content', 'user']
+        fields = ['id', 'content', 'user', 'likes']
+
+    def get_likes(self, obj):
+        return obj.likes.count()
 
     def validate_content(self, value):
         if len(value) > MAX_TWEET_LENGTH:
--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -21,7 +21,8 @@ from tweets.views import (
     tweet_delete_view,
     tweet_detail_view,
     tweet_list_view,
-    tweet_create_view
+    tweet_create_view,
+    tweet_action_view
 )
 
 urlpatterns = [
@@ -30,5 +31,6 @@ urlpatterns = [
     path('create-tweet', tweet_create_view),
     path('tweets', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
+    path('api/tweets/action', tweet_action_view),
     path('api/tweets/<int:tweet_id>/delete', tweet_delete_view)
 ]
